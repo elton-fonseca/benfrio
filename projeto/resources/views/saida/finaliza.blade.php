3d7b4051f718ba76ba56266f5b88bedb24030929
elton fonseca <elton869@gmail.com>
2020-02-28 17:40:57 -0300

    fechar botão de mensagem

--- projeto/resources/views/saida/finaliza.blade.php
+++ projeto/resources/views/saida/finaliza.blade.php
@@ -12,7 +12,7 @@
 	<!-- Modal content-->
 	<div class="modal-content">
 	  <div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal">&times;</button>
+		<button type="button" class="close" data-dismiss="modal" onclick="$('.modal').removeClass('show').addClass('fade');">&times;</button>
 		<h4 class="modal-title">AVISO</h4>
 	  </div>
 	  <div class="modal-body">
